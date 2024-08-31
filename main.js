54623b401f6e158b96aa77f996b6444bb1055b9f
Travis Salyer <84679441+TheRealGorrik@users.noreply.github.com>
2024-08-31 19:41:15 +0000

    more stuff

--- main.js
+++ main.js
@@ -6,22 +6,47 @@ const readline = require("readline")
 let playerName
 let playerClass
 
-const classArray = ["Warrior", "Sorcerer", "Ranger"]
-
+const classArray = ["", "Warrior", "Sorcerer", "Ranger"]
+const characterSheet = {
+    Warrior: {
+        health: 10,
+        attacks: {
+            slash: 2,
+            stab: 2,
+        },
+        defence: "armor"
+    },
+    Sorcerer: {
+        life: 6,
+        attack: {
+            fireball: 3,
+            icebolt: 2
+        },
+        defence: "energy"
+    },
+    Ranger: {
+        life: 6,
+        attack: {
+            trap: 1,
+            poison: 2
+        },
+        defense: "dodge"
+    }
+}
 const mainLoop = async () => {
     playerName = await askQuestion("Greetings traveler! Please enter your name: ")
 
     console.log(`Hello ${playerName}!`)
 
     console.log("Please select a class:")
-    console.log("/t1. Warrior")
-    console.log("/t2. Sorcerer")
-    console.log("/t3. Ranger")
+    console.log("  1. Warrior")
+    console.log("  2. Sorcerer")
+    console.log("  3. Ranger")
 
     while (true) {
-        playerClass = await askQuestion("")
-
-        if (typeof (playerClass) !== "number") {
+        const selection = await askQuestion("")
+        playerClass = parseInt(selection)
+        if (playerClass === NaN) {
             console.log("Please enter a value of 1, 2, or 3")
             continue
         }
@@ -32,11 +57,45 @@ const mainLoop = async () => {
         break
     }
 
-    console.log(`You have selected ${classType[playerClass]}`)
+    console.log(`You have selected the ${classArray[playerClass]} class`)
 
     const enemyClass = randomIntFromInterval(1, 3)
 
     console.log(`Your enemy class is a ${classArray[enemyClass]}`)
+
+    const player = characterSheet[classArray[playerClass]]
+    const enemy = characterSheet[classArray[enemyClass]]
+
+    const playerLife = player.life
+    const enemyLife = enemy.life
+
+    const playerAttacks = player.attacks
+    const enemyAttacks = enemy.attacks
+
+    let playerAttackDamage = 0
+
+    while (true) {
+        setTimeout(2000);
+        console.log("Your enemy is preparing to attack, what will you do?")
+        console.log("Type out your selection: ")
+        for (const attackKey in playerAttacks){
+            console.log(`  [${attackKey}] damage:${playerAttacks[attackKey]}`)
+        }
+
+        const playerAttackSelection = await askQuestion()
+        const playerAtt = playerAttack[playerAttackSelection]
+        
+        if (!playerAtt){
+            console.log("Invalid attack selected. You forego your turn.")
+        } else {
+            playerAttackDamage = playerAtt
+        }
+
+        enemyLife = enemyLife - playerAttackDamage
+
+        
+
+    }
 
 }
 
